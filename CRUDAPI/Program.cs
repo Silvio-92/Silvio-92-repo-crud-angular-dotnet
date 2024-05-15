24326364a932a55edbc10124ed6ff5e70a23ae2a
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2024-05-14 23:06:07 -0300

    teste pelo visual

--- CRUDAPI/Program.cs
+++ CRUDAPI/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 ConfigureServices(builder);
-
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
