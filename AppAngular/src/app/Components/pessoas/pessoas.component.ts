3e81476f534c57952720e0d3f3727cdc9d9b95c5
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2024-05-25 19:27:49 -0300

    Aula9 - Listagem de dados

--- AppAngular/src/app/Components/pessoas/pessoas.component.ts
+++ AppAngular/src/app/Components/pessoas/pessoas.component.ts
@@ -15,7 +15,11 @@ export class PessoasComponent implements OnInit{
   //formulario: any;
   formulario: FormGroup;
 
-  tituloFormulario:string = "";
+  tituloFormulario:string ="";
+  pessoas: Pessoa[] = [];
+
+  visibilidadeTabela:boolean = true;
+  visibilidadeFormulario:boolean = false;
 
   constructor(private pessoasService : PessoasService, private formBuilder: FormBuilder){
     this.formulario = this.formBuilder.group({ // Use formBuilder.group para inicializar o FormGroup
@@ -27,19 +31,46 @@ export class PessoasComponent implements OnInit{
   }
 
   ngOnInit(): void {
+
+    this.pessoasService.PegarTodos().subscribe(resultado => {
+      this.pessoas = resultado;
+    });
+
     this.tituloFormulario = 'Nova Pessoa';
   }
-  
+
+  ExibirFormularioCadastro(): void{
+    this.visibilidadeTabela = false;
+    this.visibilidadeFormulario = true;
+    this.tituloFormulario = 'Nova Pessoa';
+    this.formulario = this.formBuilder.group({ // Use formBuilder.group para inicializar o FormGroup
+      nome: [null, Validators.required], // Adicione validadores, se necessário
+      sobrenome: [null, Validators.required],
+      idade: [null, Validators.required],
+      profissao: [null, Validators.required]
+    });
+  }
 
   EnviarFormulario(): void {
     if (this.formulario.valid) { // Verifique se o formulário é válido antes de enviar
       const pessoa: Pessoa = this.formulario.value;
       this.pessoasService.SalvarPessoa(pessoa).subscribe(resultado => {
+        this.visibilidadeTabela = true;
+        this.visibilidadeFormulario = false;
         alert('Pessoa inserida com sucesso');
+        
+        this.pessoasService.PegarTodos().subscribe(registro => {
+          this.pessoas = registro;
+        });
       });
     } else {
       alert('Por favor, preencha todos os campos obrigatórios.');
     }
+  }
+
+  Voltar():void{
+    this.visibilidadeTabela = true;
+    this.visibilidadeFormulario = false;
   }
 
 }
